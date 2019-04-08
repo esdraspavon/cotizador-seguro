42f1b73d739b4939b1194f9039815a06439846d3
Esdras Pavon <pavonesdras@gmail.com>
2019-04-08 12:02:18 -0300

    deploy to gh-pages

---
+++ precache-manifest.151b71f1d330309dc21bc97ccf6496ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2b398a494aac00863fb5",
+    "url": "/static/js/main.2c292900.chunk.js"
+  },
+  {
+    "revision": "a9c87c0fed3693f6fb23",
+    "url": "/static/js/2.e865cd40.chunk.js"
+  },
+  {
+    "revision": "2b398a494aac00863fb5",
+    "url": "/static/css/main.d2bf2cba.chunk.css"
+  },
+  {
+    "revision": "6538db2b086252e9fe50143e273bdf79",
+    "url": "/index.html"
+  }
+];
